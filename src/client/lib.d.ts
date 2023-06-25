e54fbffded923c6c6cb27a8d4c8e01f9e7b537ed
Ralf S. Engelschall <rse@engelschall.com>
2023-06-25 23:37:44 +0200

    remove unused file

--- src/client/lib.d.ts
+++
@@ -1,6 +0,0 @@
-/*
-**  vMix-VPTZ - vMix Virtual PTZ Control
-**  Copyright (c) 2023 Dr. Ralf S. Engelschall <rse@engelschall.com>
-**  Licensed under GPL 3.0 <https://spdx.org/licenses/GPL-3.0-only>
-*/
-
