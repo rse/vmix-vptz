9f46eec551fef4c73e042cefba2597fd9d415306
Ralf S. Engelschall <rse@engelschall.com>
2023-06-26 13:57:35 +0200

    use different port

--- etc/vmix-vptz.bat
+++ etc/vmix-vptz.bat
@@ -2,6 +2,6 @@
 title vMix-VPTZ-Server
 "c:\Program Files\nodejs\node.exe" ^
     .\dst\server\index.js -v3 ^
-    -a 0.0.0.0 -p 12345 ^
+    -a 0.0.0.0 -p 12346 ^
     -A 10.0.0.21:8099 ^
     -B 10.0.0.22:8099
